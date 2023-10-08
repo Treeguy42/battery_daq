99bcee3fa2a66c4ee2984049e2cb9e1b8abfa513
Treeguy42 <54419079+Treeguy42@users.noreply.github.com>
2023-10-07 22:17:54 -0700

    Update batt_tester_RF_remote.ino
    Added OLED support, changed serial output to OLED display. OLED displays current value of duty_cycle with buttons able to change the values and send the current value to the DAQ.

--- batt_tester_RF_remote.ino
+++ batt_tester_RF_remote.ino
@@ -1,39 +1,71 @@
 #include <RF24.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET    -1
+#define OLED_I2C_ADDRESS 0x3C  //Typically 0x3C or 0x3D
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 RF24 radio(9, 10);  //CE, CSN
 const byte pins[] = {2, 3, 4, 5, 6, 7, 8};  //(Digital)D-Pin assignments for the buttons
 float duty_cycle = 0.5;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  //Initialize button pins with internal pull-up
   for (int i = 0; i < 7; i++) {
-    pinMode(pins[i], INPUT_PULLUP);  //Initialize button pins with internal pull-up
+    pinMode(pins[i], INPUT_PULLUP);  
   }
 
+  //RF radio setup
   radio.begin();
-  radio.setChannel(90);
+  radio.setAutoAck(false);
   radio.setDataRate(RF24_1MBPS);
+  radio.setPALevel(RF24_PA_MAX);
   radio.openWritingPipe(0xF0F0F0F0E1LL);
-  radio.setPALevel(RF24_PA_HIGH);
+  radio.stopListening();
+
+  //OLED setup
+  if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);  //loop forever
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
 }
 
 void loop() {
   //Check each button for changes in duty cycle
   if (digitalRead(pins[0]) == LOW) { duty_cycle -= 0.25; delay(200); } //D2
   if (digitalRead(pins[1]) == LOW) { duty_cycle += 0.25; delay(200); } //D3
-  if (digitalRead(pins[2]) == LOW) { duty_cycle -= 0.2; delay(200); } //D4
-  if (digitalRead(pins[3]) == LOW) { duty_cycle += 0.2; delay(200); } //D5
-  if (digitalRead(pins[4]) == LOW) { duty_cycle -= 0.1; delay(200); } //D6
-  if (digitalRead(pins[5]) == LOW) { duty_cycle += 0.1; delay(200); } //D7
+  if (digitalRead(pins[2]) == LOW) { duty_cycle -= 0.2; delay(200); }  //D4
+  if (digitalRead(pins[3]) == LOW) { duty_cycle += 0.2; delay(200); }  //D5
+  if (digitalRead(pins[4]) == LOW) { duty_cycle -= 0.1; delay(200); }  //D6
+  if (digitalRead(pins[5]) == LOW) { duty_cycle += 0.1; delay(200); }  //D7
 
   //Check the "send" button D8
   if (digitalRead(pins[6]) == LOW) {
-    Serial.println("Sending updated duty cycle: " + String(duty_cycle));
-    radio.stopListening();
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.print("Sending updated duty cycle:");
+    display.setCursor(0,20);
+    display.print(String(duty_cycle));
+    display.display();
+    
     radio.write(&duty_cycle, sizeof(duty_cycle));
-    radio.startListening();
     delay(200);  //Simple debounce
   }
+
+  //Continuously show current duty cycle
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print("Duty Cycle: ");
+  display.print(String(duty_cycle));
+  display.display();
 
   //Limit duty_cycle within [0.1, 1.0]
   duty_cycle = constrain(duty_cycle, 0.1, 1.0);
